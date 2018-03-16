8d1fa8ca040c3b194265b75546ad479fa46d5c0e
Vanessa de La Cuetara <vdelacuetara@gmail.com>
2018-03-16 16:01:02 -0400

    checkpoint-react-todo-events completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,7 +6,9 @@ class ToDo extends Component {
             <li>
                 <input type="checkbox" checked= {this.props.isCompleted } onChange={ this.props.toggleComplete } />
                 <span>{ this.props.description }</span>
+            <button> delete </button>
             </li>
+            
         );
     }
 }
